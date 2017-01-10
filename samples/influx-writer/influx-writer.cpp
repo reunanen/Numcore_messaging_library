dbc0a9c34399f18122cfc896a01fc5dcb04812b5
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-01-10 18:33:05 +0200

    Add influx-writer sample

---
+++ samples/influx-writer/influx-writer.cpp
@@ -0,0 +1,78 @@
+//               Copyright 2017 Juha Reunanen
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <messaging/claim/PostOffice.h>
+#include <messaging/claim/AttributeMessage.h>
+#include <numcfc/Logger.h>
+
+#include <curl/curl.h>
+
+int main(int argc, char* argv[])
+{
+	numcfc::Logger::LogAndEcho("influx-writer starting - initializing...");
+
+	numcfc::IniFile iniFile("influx-writer.ini");
+
+    claim::PostOffice postOffice;
+    postOffice.Initialize(iniFile);
+
+    postOffice.Subscribe("influx-output");
+
+    const std::string influxURL = iniFile.GetSetValue("InfluxDB", "URL", "http://localhost:8086");
+    const std::string url = (!influxURL.empty() && influxURL.back() != '/') ? influxURL + "/" : influxURL;
+
+    const std::string db = iniFile.GetSetValue("InfluxDB", "Database", "db");
+
+    numcfc::Logger::LogAndEcho("Writing to: " + url + " : " + db);
+
+    if (iniFile.IsDirty()) {
+        numcfc::Logger::LogAndEcho("Saving the ini file...");
+        iniFile.Save();
+    }
+
+    auto curl = curl_easy_init();
+
+    if (!curl) {
+        numcfc::Logger::LogAndEcho("curl_easy_init() failed", "log_error");
+        exit(1);
+    }
+
+    // create database
+    curl_easy_setopt(curl, CURLOPT_URL, (url + "query").c_str());
+    
+    const std::string createDatabase = "q=CREATE DATABASE " + db;
+
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, createDatabase.c_str());
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, createDatabase.length());
+
+    auto result = curl_easy_perform(curl);
+    if (result != CURLE_OK) {
+        numcfc::Logger::LogAndEcho("curl_easy_perform() failed: " + std::string(curl_easy_strerror(result)), "log_error");
+    }
+
+    // write data
+    curl_easy_setopt(curl, CURLOPT_URL, (url + "write?db=" + db).c_str());
+
+    while (true) {
+        slaim::Message msg;
+        while (postOffice.Receive(msg, 0.0)) {
+            if (msg.GetType() == "influx-output") {
+                claim::AttributeMessage amsg(msg);
+                for (const auto& attribute : amsg.m_attributes) {
+                    const std::string write = attribute.first + " value=" + attribute.second;
+
+                    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, write.c_str());
+                    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, write.length());
+
+                    auto result = curl_easy_perform(curl);
+                    if (result != CURLE_OK) {
+                        numcfc::Logger::LogAndEcho("curl_easy_perform() failed: " + std::string(curl_easy_strerror(result)), "log_error");
+                    }
+                }
+            }
+        }
+    }
+}
